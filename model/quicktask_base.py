049bd4a6ff4288f8b648f4f9a25f0386d4e3861c
tornixtech <tahajean-claude.kouizibo@tornixtech.com>
2024-01-05 17:56:29 +0100

    creation des differents tables

--- model/quicktask_base.py
+++ model/quicktask_base.py
@@ -5,42 +5,42 @@ from sqlalchemy.sql import expression
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Username = db.Column(db.String(128), nullable=False)
-    Email = db.Column(db.String(128), nullable=False)
-    Password = db.Column(db.String(128), nullable=False)
-    Mobile = db.Column(db.String(128), nullable=False)
-    Address = db.Column(db.String(128), nullable=False)
-    Country = db.Column(db.String(128), nullable=False)
-    City = db.Column(db.String(128), nullable=False)
+    user_username = db.Column(db.String(128), nullable=False)
+    user_email = db.Column(db.String(128), nullable=False)
+    user_password = db.Column(db.String(128), nullable=False)
+    user_mobile = db.Column(db.String(128), nullable=False)
+    user_address = db.Column(db.String(128), nullable=False)
+    user_country = db.Column(db.String(128), nullable=False)
+    user_city = db.Column(db.String(128), nullable=False)
 
 
 class Service_Provider(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    UserID = db.Column(db.Integer, db.ForeignKey('User.id'))
-    Service_details = db.Column(db.String(128), nullable=False)
+    sp_userID = db.Column(db.Integer, db.ForeignKey(User.id))
+    sp_service_details = db.Column(db.String(128), nullable=False)
 
 
 class Services(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    ServiceName = db.Column(db.String(128), nullable=False)
-    Description = db.Column(db.String(255), nullable=False)
-    Price = db.Column(db.String(128), nullable=False)
-    ProviderID = db.Column(db.Integer, db.ForeignKey('Service_Provider.id'))
+    s_serviceName = db.Column(db.String(128), nullable=False)
+    s_description = db.Column(db.String(255), nullable=False)
+    s_price = db.Column(db.String(128), nullable=False)
+    s_providerID = db.Column(db.Integer, db.ForeignKey(Service_Provider.id))
 
 
 class Booking(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    UserID = db.Column(db.Integer, db.ForeignKey('User.id'))
-    ServiceID = db.Column(db.Integer, db.ForeignKey('Services.id'))
-    BookingDate = db.Column(db.String(128), nullable=False)
-    Status = db.Column(db.String(20), nullable=False)
+    b_userID = db.Column(db.Integer, db.ForeignKey(User.id))
+    b_serviceID = db.Column(db.Integer, db.ForeignKey(Services.id))
+    b_bookingDate = db.Column(db.String(128), nullable=False)
+    b_status = db.Column(db.String(20), nullable=False)
 
 class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    UserID = db.Column(db.Integer, db.ForeignKey('User.id'))
-    BookingID = db.Column(db.Integer, db.ForeignKey('Booking.id'))
-    Amount = db.Column(db.String(128), nullable=False)
-    PaymentDate = db.Column(db.String(20), nullable=False)
+    p_userID = db.Column(db.Integer, db.ForeignKey(User.id))
+    p_bookingID = db.Column(db.Integer, db.ForeignKey(Booking.id))
+    p_amount = db.Column(db.String(128), nullable=False)
+    p_paymentDate = db.Column(db.String(20), nullable=False)
 
 
 
